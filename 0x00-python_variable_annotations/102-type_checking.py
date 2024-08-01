a500f6a202400687ddc84403e6041a63d3ed4efe
Lawson Israel Pascal <isreallawson@outlook.com>
2024-08-01 16:44:53 +0000

    Type Checking

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+Module for zooming in on an array by repeating its elements.
+
+This module contains a function that takes a tuple of items and returns a list
+where each item is repeated a specified number of times. The default repetition
+factor is 2.
+
+Arguments:
+- lst: Tuple of elements to be zoomed in on.
+- factor: Integer indicating the number of times each element should be
+repeated (default is 2).
+"""
+
+from typing import Tuple, List
+
+
+def zoom_array(lst: Tuple[int, ...], factor: int = 2) -> List[int]:
+    """
+    Zooms in on a tuple by repeating each element `factor` times.
+
+    Parameters:
+    lst (Tuple[int, ...]): A tuple containing elements to be repeated.
+    factor (int, optional): The number of times each element should be
+    repeated. Defaults to 2.
+
+    Returns:
+    List[int]: A list where each element from the tuple is repeated `factor`
+    times.
+
+    Example Usage:
+    --------------
+    >>> zoom_array((12, 72, 91))
+    [12, 12, 72, 72, 91, 91]
+
+    >>> zoom_array((12, 72, 91), 3)
+    [12, 12, 12, 72, 72, 72, 91, 91, 91]
+    """
+    zoomed_in: List[int] = [
+        item for item in lst
+        for _ in range(factor)
+    ]
+    return zoomed_in
+
+
+# Example usage
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
