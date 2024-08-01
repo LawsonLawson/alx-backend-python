12d3a9425c6e9ead5e5c5e8c5700ae31d7519f1f
Lawson Israel Pascal <isreallawson@outlook.com>
2024-08-01 14:32:39 +0000

    defined and annotated variables with the specified values

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Module for defining and annotating variables.
+
+This module defines and annotates several variables with specified values.
+The variables include an integer, a float, a boolean, and a string.
+"""
+
+
+# Variable Definitions:
+a: int = 1
+"""
+An integer variable.
+Value: 1
+"""
+
+pi: float = 3.14
+"""
+A floating-point variable.
+Value: 3.14
+"""
+
+i_understand_annotations: bool = True
+"""
+A boolean variable.
+Value: True
+"""
+
+school: str = "Holberton"
+"""
+A string variable.
+Value: 'Holberton'
+"""
